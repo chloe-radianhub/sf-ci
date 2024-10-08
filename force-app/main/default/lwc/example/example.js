bd48ab178b1ccb836271ced65436d62fdd3b81c9
chloe-radianhub <chloe-radianhub@users.noreply.github.com>
2024-10-08 14:26:39 +0000

    Apply formatting changes

--- force-app/main/default/lwc/example/example.js
+++ force-app/main/default/lwc/example/example.js
@@ -1,14 +1,14 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement, api } from "lwc";
 import addOne from "@salesforce/apex/ExampleClass.addOne";
 
 export default class Example extends LightningElement {
-    @api hi;
-    test;
+  @api hi;
+  test;
 
-    async connectedCallback() {
-        // setTimeout(async () => {
-        // this.test = await addOne(4);
-        // })
-        this.test = await addOne({num: 4});
-    }
+  async connectedCallback() {
+    // setTimeout(async () => {
+    // this.test = await addOne(4);
+    // })
+    this.test = await addOne({ num: 4 });
+  }
 }
